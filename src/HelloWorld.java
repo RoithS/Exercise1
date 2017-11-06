94ec8f3ef855b566e12978bdb73ad0f16b09504b
Sebastian Roith <ga38sow@tum.de>
2017-11-06 12:32:06 +0100

    Added a class for personalized User greetings

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
+		try {
+			String userName = askForName();
+			HelloUser user = new HelloUser(userName);
+			user.greetUser();
+		} catch (IOException e) {
+			System.err.println("Failed reading the name");
+		}
+	}
+
+	private static String askForName() throws IOException {
+		System.out.print("Please enter your name: ");
+		return new BufferedReader(new InputStreamReader(System.in)).readLine();
 	}
 
 }
